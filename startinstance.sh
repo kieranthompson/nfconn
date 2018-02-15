a0114772e907caa3c09881d173d8bfc2231ac049
Kieran Thompson <l00118651@student.lyit.ie>
2018-02-15 16:33:09 +0000

    added script to start instance

---
+++ startinstance.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# final year project ec2 instance intialisation script
+echo 'starting final year project ec2 instance'
+ssh -i /Users/kieran/Downloads/final_year_project_key.pem ubuntu@ec2-54-88-11-212.compute-1.amazonaws.com
