bcc7982e256ce6997e115f23e3ac1b41e9f7827c
Kieran Thompson <l00118651@student.lyit.ie>
2018-04-21 16:01:55 +0100

    updated routes to include getwifi()

--- routes/routes.js
+++ routes/routes.js
@@ -6,5 +6,6 @@ var router = express.Router();
 router.post('/authenticate', actions.authenticate);
 router.post('/adduser', actions.addNew);
 router.get('/getinfo', actions.getinfo);
+router.get('/getWifi', actions.getWifi);
 
 module.exports = router;
